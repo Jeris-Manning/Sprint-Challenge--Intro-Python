912a8c452eb0b3e377fb5e2fbf8cfca199fa607a
Jeris Manning <the.goose.moose@gmail.com>
2020-03-13 11:00:26 -0600

    oop segment and first part of csv segment complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,6 +18,23 @@
 #
 # Put a comment noting which class is the base class
 
-
 class Vehicle(): #Base Class
+  pass
+
+class GroundVehicle(Vehicle):
+  pass
+
+class FlightVehicle(Vehicle):
+  pass
+
+class Car(GroundVehicle):
+  pass
+
+class Motorcycle(GroundVehicle):
+  pass
+
+class Airplane(FlightVehicle):
+  pass
+
+class Starship(FlightVehicle):
   pass
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,19 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
+    def drive(self):
+        return "vroooom"
 
-    # TODO
+# TODO
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
+    def drive(self):
+        return "BRAAAP!!"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -30,3 +39,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for bigTimeMachines in vehicles:
+    print(bigTimeMachines.drive())
